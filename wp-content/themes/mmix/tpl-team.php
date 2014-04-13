371c5e296707c0c8adc5db4b3da36b2ade9fbfdb
Sariha Chabert <schabert@beauteprivee.com>
2014-04-13 15:38:49 -0400

    team page

--- wp-content/themes/mmix/tpl-team.php
+++ wp-content/themes/mmix/tpl-team.php
@@ -6,63 +6,42 @@
 
 <div>
 
-  <h2 class="alt-title robotoFont">Core Team</h2>
-  <div class="clearfix"></div>
+  <?php
+  //set the team you want to show here :
+  $groups =  array(
+    'coreTeam' => array('name'=>'Core Team'),
+    'benevole' => array('name'=>'Benevoles'),
+    'ambassadeur'=> array('name'=>'Ambassadeurs')
+  );
+  ?>
 
-  <?php $users = get_users(array('meta_key' => 'mmix_team', 'meta_value' => 'coreTeam')) ?>
-  <?php foreach($users as $user): ?>
-    <?php $metas = array_map( function( $a ){ return $a[0]; }, get_user_meta( $user->ID ) ); ?>
-  <div class="col-md-4">
-    <div class="user-panel">
-      <div class="user-avatar">
-        <?php echo get_avatar( $user->ID, 180 ); ?>
-      </div>
-      <h3><?php echo $metas['first_name']; ?> <?php echo $metas['last_name']; ?></h3>
-      <?php if(!empty($metas)): ?>
-      <div class="twiter-account"><?php echo $metas['twitter']; ?></div>
-      <?php endif; ?>
-    </div>
-  </div>
-  <?php endforeach; ?>
-
-  <div class="clearfix"></div>
-  <h2 class="alt-title robotoFont"><?php echo __("[en]Volunteers[/en][fr]Bénévoles[/fr]"); ?></h2>
-  <div class="clearfix"></div>
-
-  <?php $users = get_users(array('meta_key' => 'mmix_team', 'meta_value' => 'benevole')) ?>
-  <?php foreach($users as $user): ?>
-    <?php $metas = array_map( function( $a ){ return $a[0]; }, get_user_meta( $user->ID ) ); ?>
-    <div class="col-md-3">
-      <div class="user-panel">
-        <div class="user-avatar">
-          <?php echo get_avatar( $user->ID, 180 ); ?>
+  <?php foreach($groups as $group=>$value) :  ?>
+  <?php $users = get_users(array('meta_key' => 'mmix_team', 'meta_value' => $group)) ?>
+    <?php if(!empty($users)): ?>
+    <h2 class="alt-title robotoFont"><?php echo $value['name']; ?></h2>
+    <div class="clearfix"></div>
+      <?php foreach($users as $user): ?>
+        <?php $metas = array_map( function( $a ){ return $a[0]; }, get_user_meta( $user->ID ) ); ?>
+      <div class="<?php echo ($group=='coreTeam') ? 'col-md-4' : 'col-md-3'; ?>">
+        <div class="user-panel">
+          <div class="user-avatar">
+            <?php echo get_avatar( $user->ID, 180 ); ?>
+          </div>
+          <h3><?php echo $metas['first_name']; ?> <?php echo $metas['last_name']; ?></h3>
+          <?php if(!empty($metas)): ?>
+          <div class="twiter-account">
+            <a href="http://www.twitter.com/<?php echo $metas['twitter']; ?>" target="_blank">
+              <i class="fa fa-twitter"></i> <?php echo $metas['twitter']; ?>
+            </a>
+          </div>
+          <?php endif; ?>
         </div>
-        <h3><?php echo $metas['first_name']; ?> <?php echo $metas['last_name']; ?></h3>
-        <?php if(!empty($metas)): ?>
-          <div class="twiter-account"><?php echo $metas['twitter']; ?></div>
-        <?php endif; ?>
       </div>
-    </div>
+      <?php endforeach; ?>
+    <?php endif; ?>
   <?php endforeach; ?>
 
-  <div class="clearfix"></div>
-  <h2 class="alt-title robotoFont"><?php echo __("[en]Ambassadors[/en][fr]Ambassadeurs[/fr]"); ?></h2>
-  <div class="clearfix"></div>
 
-  <?php $users = get_users(array('meta_key' => 'mmix_team', 'meta_value' => 'ambassador')) ?>
-  <?php foreach($users as $user): ?>
-    <?php $metas = array_map( function( $a ){ return $a[0]; }, get_user_meta( $user->ID ) ); ?>
-    <div class="col-md-3">
-      <div class="user-panel">
-        <div class="user-avatar">
-          <?php echo get_avatar( $user->ID, 180 ); ?>
-        </div>
-        <h3><?php echo $metas['first_name']; ?> <?php echo $metas['last_name']; ?></h3>
-        <?php if(!empty($metas)): ?>
-          <div class="twiter-account"><?php echo $metas['twitter']; ?></div>
-        <?php endif; ?>
-      </div>
-    </div>
-  <?php endforeach; ?>
+
 
 </div>
