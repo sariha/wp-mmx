b6c30450ba86d1a3a6f948d212415f8a7e8282f0
Sariha Chabert <schabert@beauteprivee.com>
2014-05-14 20:33:05 -0400

    share with addthis

--- wp-content/themes/mmix/home.php
+++ wp-content/themes/mmix/home.php
@@ -28,9 +28,32 @@
 
             </div>
             <div class="col-md-10 col-sm-10 col-xs-8">
-              <div class="entry">
-                <?php the_content('<span class="fa fa-arrow-circle-right fa-3x"></span>'); ?>
+
+              <div class="row">
+                <div class="col-md-12">
+                  <p style="margin: 5px auto">
+                    <!-- AddThis Button BEGIN -->
+                  <div class="addthis_toolbox addthis_default_style addthis_16x16_style">
+                    <a class="addthis_button_facebook"></a>
+                    <a class="addthis_button_twitter"></a>
+                    <a class="addthis_button_google_plusone_share"></a>
+                    <a class="addthis_button_pinterest_share"></a>
+                    <a class="addthis_button_compact"></a><a class="addthis_counter addthis_bubble_style"></a>
+                  </div>
+                  <script type="text/javascript">var addthis_config = {"data_track_addressbar":true};</script>
+                  <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5374079b313142e4"></script>
+                  <!-- AddThis Button END -->
+                  </p>
+                </div>
               </div>
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="entry">
+                    <?php the_content('<span class="fa fa-arrow-circle-right fa-3x"></span>'); ?>
+                  </div>
+                </div>
+              </div>
+
             </div>
           </div>
 
